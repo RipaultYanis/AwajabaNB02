0b002e174547f3fec95d24548e175f377ce82b27
Yanis <etudiant@debian9-gui.sio.lla.fr>
2019-10-08 16:19:15 +0200

    Commit n1

--- src/com/awa/awajaba/metier/Repas.java
+++ src/com/awa/awajaba/metier/Repas.java
@@ -5,10 +5,19 @@
  */
 package com.awa.awajaba.metier;
 
+import java.time.LocalDate;
+
 /**
  *
  * @author etudiant
  */
 public class Repas {
     
+	private int numero;
+	private LocalDate date;
+	
+	Repas(int numero, LocalDate date){
+	this.numero = numero;
+	this.date = date;
+        }
 }
